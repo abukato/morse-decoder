17f0d2eaa9afb112163b7c16aa68d9cf2dbace58
abukato <artur.bukato@gmail.com>
2020-03-05 12:04:22 +0300

    feat: task morse-decoder done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let regexp = /.{1,10}/g;
+    let arr = expr.match(regexp);
+    let result = '';
+  
+    for (let i = 0; i < arr.length; i++) {
+      let char = '';
+      for (let j = 0; j < arr[i].length; j += 2) {
+        if (arr[i][j]+arr[i][j+1] === '10') {
+          char += '.' 
+        } else if (arr[i][j]+arr[i][j+1] === '11') {
+          char += '-';
+        } else if (arr[i][j]+arr[i][j+1] === '**') {
+          char += '';
+        }
+      }  
+      if (char === '') {
+        result += ' ';
+      } else {
+        result += MORSE_TABLE[char]
+      }
+    }
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode("00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010"));
